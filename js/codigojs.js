8fe09d53d742492127755e4cb78c86fdc8cd9444
agusbruzzi <agusb.bruzzi@gmail.com>
2022-04-12 22:26:39 -0300

    2da version de la tienda interactiva. Se finalizan casi todas las funciones y se termina de definir los estilos usados

--- js/codigojs.js
+++ js/codigojs.js
@@ -1,34 +1,58 @@
-// ADMINISTRACIÓN DE PRODUCTOS - ABM PRODUCTOS
-// Cargo productos genéricos
-function Producto (id, nombre, precio, estado) {
+// Cargo productos genéricos y muestro los mismos en el html
+function Producto (id, nombre, precio, foto, estado) {
     this.idproducto = id;
     this.nomproducto = nombre;
     this.precio = precio;
+    this.foto = foto;
     this.publicado = estado;
 }
 const productos = [
-    {idproducto: 1, nomproducto: "Producto 1", precio: 1230, publicado: true },
-    {idproducto: 2, nomproducto: "Producto 2", precio: 780, publicado: true },
-    {idproducto: 3, nomproducto: "Producto 3", precio: 940, publicado: false },
-    {idproducto: 4, nomproducto: "Producto 4", precio: 365, publicado: true },
-    {idproducto: 5, nomproducto: "Producto 5", precio: 1870, publicado: true },
-    {idproducto: 6, nomproducto: "Producto 6", precio: 2345, publicado: true },
+    {idproducto: 1, nomproducto: "LASAGNA BOLOGNESA CONGELADA", precio: 920, foto: "../assets/img/lasagna.jpg" ,publicado: true },
+    {idproducto: 2, nomproducto: "ÑOQUIS CASEROS CONGELADOS", precio: 820, foto: "../assets/img/ñoquis.jpg" , publicado: true },
+    {idproducto: 3, nomproducto: "PICADA GRANDE - 6 PICAN Ó 4 CENAN", precio: 3600, foto: "../assets/img/picada.jpg" , publicado: true },
+    {idproducto: 4, nomproducto: "SEMANA LABORAL - 5 COMIDAS", precio: 4850, foto: "../assets/img/combo5.jpg" , publicado: true },
+    {idproducto: 5, nomproducto: "SEMANA LIGHT - CUIDATE SIN ESFUERZO!!", precio: 4800, foto: "../assets/img/combolight.jpg" , publicado: true },
+    {idproducto: 6, nomproducto: "BIFES A LA CRIOLLA - PARA DOS PERSONAS", precio: 1200, foto: "../assets/img/bifes.jpg" , publicado: true },
+    
 ]
 const elementopadre = document.getElementById("listadoproductos");
-for (const producto of productos) {
+function renderizarArray(array){
+    array.forEach((producto) => {
         let contenedor = document.createElement('div');
-        contenedor.innerHTML =  `<img src="../assets/img/servicios-organizacion-integral-1.jpg" class="fototienda" >
+        contenedor.innerHTML =  `
+                                <img src="${producto.foto}" class="fototienda" >
                                  <h4> ${producto.nomproducto} </h4>
-                                 <p>descripcion del productodescripcion del productodescripcion del producto</p>
                                  <p class="precio"> PRECIO: ${producto.precio}</p>
                                  <div class="row">
-                                    <div class="col"><button class="botoncarrito" onclick="botonRestarUno()">-</button></div>
-                                    <div class="col"><p class="mostradorcantidad" id="mostradorCantidad"> 0 </p></div>
-                                    <div class="col"><button type="button" class="botoncarrito" onclick="botonSumarUno()">+</button></div>
+                                    <div class="col"><button class="botoncarrito" onclick="botonRestarUno(${producto.idproducto})">-</button></div>
+                                    <div class="col"><p class="mostradorCantidad" id="mostradorCantidad"> 0 </p></div>
+                                    <div class="col"><button type="button" class="botoncarrito" onclick="botonSumarUno(${producto.idproducto})">+</button></div>
                                  </div>
-                                 <button class="botonsumarcarrito" onclick="agregarAlCarrito">AGREGAR AL CARRITO</button>`;
+                                 <button class="botonsumarcarrito" onclick="agregarAlCarrito(${producto.idproducto})">AGREGAR AL CARRITO</button>`;
+        contenedor.setAttribute('class',"col-md-3 productosmenu");
         elementopadre.appendChild(contenedor);
-    }
+    })
+}
+//Solamente muestro los productos que posean el atributo publicado igual a true
+ const productosPublicados = productos.filter((el) => el.publicado == true);
+ renderizarArray(productosPublicados);
+
+//Realizo lo mismo pero para los productos que se agregan al carrito y deben mostrarse a la izquierda en el html
+const elementopadreCarrito = document.getElementById("listadocarrito");
+function renderizarCarrito(producto){
+        var nombreprod = productosPublicados[(producto.idproducto)-1].nomproducto; 
+        let contenedor = document.createElement('div');
+        contenedor.innerHTML =  `
+                                 <h5 class="nomcarrito"> ${nombreprod} </h5>
+                                 <p class="infocarrito" id="cantidad${producto.idproducto}"> CANTIDAD: ${producto.cantidad}</p>
+                                 <p class="infocarrito preciocarrito" id="precio${producto.idproducto}"> PRECIO: ${producto.precio}</p>
+                                 <button class="botonborrardelcarrito" onclick="borrarDelCarrito(${producto.idproducto})">BORRAR DEL CARRITO</button>`;
+                                 
+        contenedor.setAttribute('class',"productoListado");
+        contenedor.setAttribute('id',"productoListado"+producto.idproducto);
+        elementopadreCarrito.appendChild(contenedor);
+}
+
 // MODIFICAMOS ELEMENTOS HTML DE LA PÁGINA TIENDA
 //Aumentamos "+1" cada vez que se presiona el botón "+"
 function sumarUno(cantidadActual) {
@@ -38,9 +62,9 @@ function sumarUno(cantidadActual) {
 }
 function botonSumarUno(ordenElemento) {
     let cantidades = document.getElementsByClassName("mostradorCantidad");
-    let cantidadActual = parseInt(cantidades[ordenElemento].innerHTML);
+    let cantidadActual = parseInt(cantidades[ordenElemento-1].innerHTML);
     proximaCantidad = sumarUno(cantidadActual);
-    document.getElementsByClassName("mostradorCantidad")[ordenElemento].innerHTML = proximaCantidad;
+    document.getElementsByClassName("mostradorCantidad")[ordenElemento-1].innerHTML = proximaCantidad;
 }
 //Disminuimos "-1" cada vez que se presiona el botón "-"
 function restarUno(cantidadActual) {
@@ -53,208 +77,88 @@ function restarUno(cantidadActual) {
 }
 function botonRestarUno(ordenElemento) {
     let cantidades = document.getElementsByClassName("mostradorCantidad");
-    let cantidadActual = parseInt(cantidades[ordenElemento].innerHTML);
+    let cantidadActual = parseInt(cantidades[ordenElemento-1].innerHTML);
     proximaCantidad = restarUno(cantidadActual);
-    document.getElementsByClassName("mostradorCantidad")[ordenElemento].innerHTML = proximaCantidad;
+    document.getElementsByClassName("mostradorCantidad")[ordenElemento-1].innerHTML = proximaCantidad;
 }
 // Ahora gestionamos el carrito
-/*Creo el objeto carrito
+//Creo el objeto carrito
 function ProductoCarrito (idproducto, cantidad, precio) {
     this.idproducto = idproducto;
     this.cantidad = cantidad;
     this.precio = precio;
 }
-// Funcion para listar los objetos del carrito
-function crearListaCarrito(lista, id, nombre, cantidad, precio){
-    lista = lista + "ID: "+id + " - ";
-    lista = lista + "NOMBRE: "+nombre + " - ";
-    lista = lista + "CANTIDAD: "+cantidad + " - ";
-    lista = lista + "PRECIO : "+precio + " - ";
-    lista = lista + "\n";
-    return lista;
-}*/
 // Boton Agregar al Carrito
-function agregarAlCarrito(precio, ordenElemento) {
+const carrito = [];
+function agregarAlCarrito(ordenElemento) {
     let cantidades = document.getElementsByClassName("mostradorCantidad");
-    let cantidadActual = parseInt(cantidades[ordenElemento].innerHTML);
-    let preSubTotal = cantidadActual * precio;
-    let anteriorSubtotal = parseInt(document.getElementById("totalcarrito").innerHTML);
-    let nuevoSubtotal = preSubTotal + anteriorSubtotal;
-    document.getElementById("totalcarrito").innerHTML = nuevoSubtotal;
-    document.getElementsByClassName("mostradorCantidad")[ordenElemento].innerHTML = 0;
+    let cantidadActual = parseInt(cantidades[ordenElemento-1].innerHTML);
+    //Verificamos que el cliente efectivamente haya seleccionado el producto, para evitar tener en el carrito productos con cantidad 0
+    if (cantidadActual !=0) {
+        //Aca verificamos si es un producto nuevo que agrega al carrito o ya lo agregó antes, para no tener en el carrito varias veces el 
+        //mismo producto.
+        if (carrito.some((el) => el.idproducto == ordenElemento) == false ) {
+            //En este caso es un producto nuevo asi que creamos el objeto y lo pusheamos al array carrito
+            let precio = productosPublicados[ordenElemento-1].precio;
+            let preSubTotal = cantidadActual * precio;
+            let anteriorSubtotal = parseInt(document.getElementById("totalcarrito").innerHTML);
+            let nuevoSubtotal = preSubTotal + anteriorSubtotal;
+            document.getElementById("totalcarrito").innerHTML = nuevoSubtotal;
+            document.getElementsByClassName("mostradorCantidad")[ordenElemento-1].innerHTML = 0;
+
+            var productoAgregado = new ProductoCarrito (ordenElemento, cantidadActual,preSubTotal);
+            carrito.push(productoAgregado);
+            renderizarCarrito(productoAgregado);
+        }
+        else {
+            //En este caso el cliente solo estaría modificando la cantidad que quiere de cierto producto
+            let existente = carrito.find((el) => el.idproducto == ordenElemento);
+            let nuevaCantidad = cantidadActual + existente.cantidad;
+            let precio = productosPublicados[ordenElemento-1].precio;
+            let preSubTotal = cantidadActual * precio;
+            let nuevoPrecio = existente.precio + preSubTotal;
+
+            let anteriorSubtotal = parseInt(document.getElementById("totalcarrito").innerHTML);
+            let nuevoSubtotal = preSubTotal + anteriorSubtotal;
+            document.getElementById("totalcarrito").innerHTML = nuevoSubtotal;
+            document.getElementsByClassName("mostradorCantidad")[ordenElemento-1].innerHTML = 0;
+
+            existente.cantidad = nuevaCantidad;
+            existente.precio = nuevoPrecio;
+            document.getElementById("cantidad"+existente.idproducto).innerHTML= "CANTIDAD: "+nuevaCantidad;
+            document.getElementById("precio"+existente.idproducto).innerHTML= "PRECIO: "+nuevoPrecio;
+        }     
+    } 
+    else {
+        document.getElementById("totalcarrito").innerHTML = 0;
+    }
 }
 //Boton Borrar Carrito
 function borrarCarrito(){
     document.getElementById("totalcarrito").innerHTML = 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-//FUNCIONES AUN NO APLICADAS
-/*
-
-
-// Añadir nuevos productos
-function crearNuevoProducto (){
-    var nombreNuevoProducto = prompt("Ingrese nombre del nuevo producto: ");
-    var precioNuevoProducto = prompt("Ingrese precio del nuevo producto: ");
-    Para calcular el ID del nuevo producto, siendo que la idea es que sean consecutivos,
-    se me ocurrio en primera instancia crear un nuevo array pero que solo contengan los id 
-    const idproductosExistentes = productos.map ((pr) => pr.idproducto);
-    /* una vez que ya tengo ese array, lo ordeno de mayor a menor, teniendo en la posición 0
-    al id más grande
-    idproductosExistentes.sort((a, b) => b - a);
-    var idNuevoProducto = idproductosExistentes[0] + 1;
-    var estadoNuevoProducto = true;
-    var productoNuevo = new Producto (idNuevoProducto, nombreNuevoProducto, precioNuevoProducto, estadoNuevoProducto);
-    productos.push(productoNuevo);
-}
-/* Ahora voy hacer unas funciones que muestren las listas de los productos
-si cumplen ciertos requisitos, por eso cree la funcion crearlista() para 
-no repetir código
-function crearLista(lista, id, nombre, precio){
-    lista = lista + "ID: "+id + " - ";
-    lista = lista + "NOMBRE: "+nombre + " - ";
-    lista = lista + "PRECIO : "+precio + " - ";
-    lista = lista + "\n";
-    return lista;
-}
-// Mostrar todos los productos
-function mostrarProductos(){
-    var listaCompleta = "PRODUCTOS: "+"\n";
-    for (const producto of productos) {
-        var listaCompleta = crearLista (listaCompleta, producto.idproducto, producto.nomproducto, producto.precio);
-    }
-    alert(listaCompleta);
-}
-// Mostrar Productos Actualmente en Venta
-function mostrarProductosEnVenta(){
-    var listaCompleta = "PRODUCTOS EN VENTA: "+"\n";
-    for (const producto of productos) {
-        if (producto.publicado == true){
-            var listaCompleta = crearLista (listaCompleta, producto.idproducto, producto.nomproducto, producto.precio);
-        }
-    }
-    alert(listaCompleta);
-}
-// Mostrar Productos Actualmente NO en Venta
-function mostrarProductosNoEnVenta(){
-    var listaCompleta = "PRODUCTOS NO EN VENTA: "+"\n";
-    for (const producto of productos) {
-        if (producto.publicado == false){
-            var listaCompleta = crearLista (listaCompleta, producto.idproducto, producto.nomproducto, producto.precio);
-        }
-    }
-    alert(listaCompleta);
-}
-// Editar productos
-function editarProductos(){
-    var valorID = prompt("A continuación está por editar productos."+"\n"+
-                        "Ingrese el ID del producto que desea modificar: ");
-    var productoElegido = productos.find ((prod) => prod.idproducto == valorID);
-    var lista = "";
-    var confirmacion = prompt("¿El articulo que quiere editar es el siguiente?" + "\n" + crearLista(lista,productoElegido.idproducto,productoElegido.nomproducto,productoElegido.precio)+ "Ingrese S: Sí // N: No");
-    if (confirmacion === "S" || confirmacion === "s") {
-        var valorModif = prompt ("A continuación Ingrese el Valor que desea modificar :" + "\n"+
-                             "N: Nombre del producto"+"\n" +
-                             "P: Precio del procucto" + "\n" +
-                             "E: Estado del Producto" + "\n" +
-                             "Pulse cualquier otra tecla para cancelar.");
-        if (valorModif === "N" || valorModif === "n"){
-            var nuevoNombre = prompt("Ingrese nuevo nombre: ");
-            productoElegido.nomproducto = nuevoNombre;
-            alert("Se modificaron los datos correctamente.")
-        }
-        else if (valorModif === "P" || valorModif === "P"){
-            var nuevoPrecio = prompt("Ingrese nuevo precio: ");
-            productoElegido.precio = nuevoPrecio;
-            alert("Se modificaron los datos correctamente.")
-        }
-        else if (valorModif === "E" || valorModif === "e"){
-            var estadoProd = productoElegido.publicado;
-            if (estadoProd == true) {
-                var conf = prompt ("Actualmente su producto se encuentra a la venta. ¿Desea quitarlo?"+"\n"+"Ingrese S para continuar o cualquier otra tecla para cancelar la operación.");
-                if (conf === "S" || conf === "s"){
-                    productoElegido.publicado = false;
-                    alert("Se modificaron los datos correctamente.")
-                }
-                else {
-                    alert("Operación Cancelada");
-                }
-            }
-            else {
-                var conf = prompt ("Actualmente su producto se encuentra fuera de venta. ¿Desea volver a ponerlo en venta?"+"\n"+"Ingrese S para continuar o cualquier otra tecla para cancelar la operación.");
-                if (conf === "S" || conf === "s"){
-                    productoElegido.publicado = true;
-                    alert("Se modificaron los datos correctamente.")
-                }
-                else {
-                    alert("Operación Cancelada");
-                }
-            }
-        }
-        else {
-            alert("Operación Cancelada");
-        }
-    }
-    else {
-        alert("Operación Cancelada");
+    //Borramos todos los elementos que pertenecen al array carrito
+    var cantidadCarrito = carrito.length;
+    carrito.splice(0,cantidadCarrito);
+    //Borramos el listado mostrado en html
+    let productosListados = document.getElementsByClassName("productoListado");
+    var cantidadAborrar = productosListados.length;
+    for  (let i = 0; i<cantidadAborrar; i++){
+        productosListados[0].remove();
     }
 }
-//ADMINISTRACIÓN DE VENTAS
-/* Con esta función hago la compra, faltan muchas verificaciones con lo que ingresa el cliente 
-pero, como la idea es que se haga a futuro con el front que ya diseñe, hice las necesarias
-no más. 
-function generarCarrito(){
-    var alerta = true;
-    var carrito = [];
-    while (alerta == true){
-        idSeleccionado = prompt ("Los articulos actualmente disponibles son: "+ mostrarProductosEnVenta()+ "Seleccione ID del producto que desea comprar. Si desea cancelar toda la operación ingrese 'S'. ");
-        if (idSeleccionado == "S" || idSeleccionado == "s"){
-            alert("Operación Cancelada");
-            alerta = false;
-        }
-        else{
-            var productoElegido = productos.find ((prod) => prod.idproducto == idSeleccionado);
-            var lista = "";
-            var cant = parseInt(prompt("Articulo Seleccionado: " + "\n" + crearLista(lista,productoElegido.idproducto,productoElegido.nomproducto,productoElegido.precio)+ "Ingrese la cantidad que desea comprar: "));
-            precioparcial = cant * productoElegido.precio;
-            var productoAgregado = new ProductoCarrito (productoElegido.idproducto, cant, precioparcial);
-            carrito.push(productoAgregado);
-            cons =  prompt("¿Desea agregar más productos a su carrito?" + "\n" + "Ingrese 'S' si desea agregar más productos o 'N' si desea continuar con el pago.");
-            if (cons == "N" || cons =="n"){
-                var listaCompleta = "DETALLE DE SU CARRITO: "+"\n";
-                for (const producto of carrito) {
-                    var productoBD = productos.find ((prod) => prod.idproducto == producto.idproducto);
-                    var nompr = productoBD.nomproducto;
-                    var listaCompleta = crearListaCarrito (listaCompleta, producto.idproducto, nompr, producto.cantidad, producto.precio);
-                }
-                    const subtotales = carrito.map ((pr) => pr.precio);
-                    var totalApagar = 0;
-                    for (const subtotal of subtotales) {
-                        totalApagar = totalApagar + subtotal;
-                    }
-                    listaCompleta = listaCompleta + "\n" + "TOTAL A PAGAR: " + totalApagar;
-                    alert(listaCompleta);
-                    alerta = false;
-            }
-        }
+//Boton Borrar Producto del Carrito
+function borrarDelCarrito(idprod) {
+    let existente = carrito.find((el) => el.idproducto == idprod)
+    let precioAdescontar = existente.precio;
+    let anteriorSubtotal = parseInt(document.getElementById("totalcarrito").innerHTML);
+    let nuevoSubTotal = anteriorSubtotal - precioAdescontar;
+    document.getElementById("totalcarrito").innerHTML = nuevoSubTotal;
 
-        
-
-    }
-}*/
-
+    indice = carrito.indexOf(existente);
+    carrito.splice(indice,1);
+    let divEliminar = document.getElementById("productoListado"+existente.idproducto);
+    divEliminar.remove();
+}
 
 
 
